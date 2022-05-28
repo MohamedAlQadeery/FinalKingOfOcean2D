bd4fde2b4de8ffd29eb4469ea1e31483845436c8
abood <abood.qew@gmail.com>
2022-05-28 17:10:31 +0300

    Work on FishingTimer
    Close as To solation
    but not yet sad T_T

--- Assets/TimeQuitTest.cs
+++ Assets/TimeQuitTest.cs
@@ -8,52 +8,41 @@ public class TimeQuitTest : MonoBehaviour
 {
     [SerializeField] TMP_Text textCount;
     int count = 1;
+    DateTime quitDateTime1;
+    string dateQuitString;
     DateTime quitDateTime;
+    int timeLeft;
     // Start is called before the first frame update
     void Start()
     {
+        
         string dateQuitString = PlayerPrefs.GetString("quitDateTime", "");
-        quitDateTime = DateTime.Parse(dateQuitString);
-        Debug.Log("App Start");
-        Debug.Log("Time Start : " + (int)(DateTime.Now - quitDateTime).TotalSeconds);
-        Debug.Log("Time Start1 : " + quitDateTime.Second);
-        //Debug.Log("Test !> : " + (int)(DateTime.Now - quitDateTime).TotalSeconds);
+        string countString = PlayerPrefs.GetString("Count", "");
+        count = int.Parse(countString);       
+        quitDateTime = DateTime.Parse(dateQuitString);       
+        timeLeft = ((int)(DateTime.Now - quitDateTime).TotalSeconds) - 43200;
+        count = count + timeLeft;
+
+        Debug.Log(((int)(DateTime.Now - quitDateTime).TotalSeconds) - 43200);
+        Debug.Log(countString);
+
         if (!dateQuitString.Equals("")) {
             StartCoroutine("Count");
         }
     }
-
     IEnumerator Count()
-    {
+    {     
         while (true)
-        {
-            count = (int)(DateTime.Now - quitDateTime).TotalSeconds;
+        {          
             textCount.text = (count++).ToString();
             yield return new WaitForSeconds(1f);
         }
     }
-    private void OnApplicationPause(bool pause)
-    {
-    }
-
     private void OnApplicationQuit()
     {
-       // Debug.Log("Time Quit : " + quitDateTime.Second);
-       // Debug.Log("Time Quit : " + (int)(DateTime.Now - quitDateTime).TotalSeconds);
         DateTime quitDate= DateTime.Now;        
         PlayerPrefs.SetString("quitDateTime", quitDate.ToString());
-       // Debug.Log("App Quit1");
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PlayerPrefs.SetString("Count",count.ToString());
     }
 
-    public void ButtonClick()
-    {
-        SoundManager.Instance.PlaySound(SoundManager.Sound.ButtonSonud);
-        Debug.Log("Time now :" + (int)(DateTime.Now - quitDateTime).TotalSeconds);
-        
-    }
 }
