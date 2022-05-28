da66794f4238a7fae822d3a4c730bcbc6d6af1d6
Marah-Nabil <105675270+marah-nabil@users.noreply.github.com>
2022-05-28 22:01:31 +0300

    2Panel_Guild && Chat UI

--- Assets/Assets Pack/GUI PRO Kit - Fantasy RPG/Scripts/PanelFantasyRPG.cs
+++ Assets/Assets Pack/GUI PRO Kit - Fantasy RPG/Scripts/PanelFantasyRPG.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+
 namespace LayerLab.FantasyRPG
 {
     public class PanelFantasyRPG : MonoBehaviour
     {
         [SerializeField] private GameObject[] otherPanels;
-        [SerializeField] private GameObject signUp;
+
         public void OnEnable()
         {
             for (int i = 0; i < otherPanels.Length; i++) otherPanels[i].SetActive(true);
@@ -17,30 +16,6 @@ namespace LayerLab.FantasyRPG
         public void OnDisable()
         {
             for (int i = 0; i < otherPanels.Length; i++) otherPanels[i].SetActive(false);
-        }
-
-        public void Close()
-        {
-            Destroy(gameObject);
-        }
-
-        public void SignUp()
-        {
-            Close();
-            GameObject newUiSginUp = Instantiate(signUp, transform.position, transform.rotation) as GameObject;
-            newUiSginUp.transform.SetParent(GameObject.FindGameObjectWithTag("UILogin").transform, false);
-        }
-
-        public void Login()
-        {
-            Close();
-            GameObject newUiLogin = Instantiate(signUp, transform.position, transform.rotation) as GameObject;
-            newUiLogin.transform.SetParent(GameObject.FindGameObjectWithTag("UILogin").transform, false);
-        }
-
-        public void LoginButton()
-        {
-            SceneManager.LoadScene(1);
         }
     }
 }
